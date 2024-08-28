5d67267f8094884ed8d740ec4013950c72eec75e
Robert França <robertalan.dsf@gmail.com>
2024-08-28 02:27:18 -0300

    Implementacao de repository.

--- src/main/java/repository/LivroRepositoryImplement.java
+++ src/main/java/repository/LivroRepositoryImplement.java
@@ -2,61 +2,177 @@ package repository;
 
 import models.Livro;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LivroRepositoryImplement implements LivroRepository {
+    private static final String URL = "jdbc:mysql://localhost:3306/Biblioteca";
+    private static final String USER = "root";
+    private static final String PASSWORD = "12345";
 
-    private EntityManagerFactory entityManagerFactory;
-    private EntityManager entityManager;
 
-    public LivroRepositoryImplement() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("livraria");
-        entityManager = entityManagerFactory.createEntityManager();
+    public void testConnection() throws RepositoryException{
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            if (conn != null) {
+                System.out.println("Connection to the database was successful!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            System.out.println("An error occurred while connecting to the database:");
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void salvar(Livro livro) {
-        entityManager.getTransaction().begin();
-        entityManager.persist(livro);
-        entityManager.getTransaction().commit();
+    public void salvar(Livro livro) throws RepositoryException {
+        String sql = "INSERT INTO livro (titulo, autor, editora, status, nota, anotacao, isbn) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, livro.getTitulo());
+            stmt.setString(2, livro.getAutor());
+            stmt.setString(3, livro.getEditora());
+            stmt.setString(4, livro.getStatus());
+            stmt.setInt(5, livro.getNota());
+            stmt.setString(6, livro.getAnotacao());
+            stmt.setString(7, livro.getIsbn());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+//            throw new RuntimeException("ERROR", e);
+            throw new RepositoryException("Error saving book", e);
+        }
+
     }
 
-    @Override
-    public Livro buscarPorTitulo(String titulo) {
-        TypedQuery<Livro> query = entityManager.createQuery("SELECT l FROM Livro l WHERE l.titulo = :titulo", Livro.class);
-        query.setParameter("titulo", titulo);
-        return query.getSingleResult();
+    public Livro buscarPorTitulo(String titulo) throws RepositoryException {
+        String sql = "SELECT * FROM livro WHERE titulo = ?";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, titulo);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+
+                return new Livro(
+                		rs.getInt("id"),
+                        rs.getString("titulo"),
+                        rs.getString("autor"),
+                        rs.getString("editora"),
+                        rs.getString("status"),
+                        rs.getInt("nota"),
+                        rs.getString("anotacao"),
+                        rs.getString("isbn")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+    }
+        return null;
     }
 
-    @Override
-    public Livro buscarPorIsbn(String isbn) {
-        TypedQuery<Livro> query = entityManager.createQuery("SELECT l FROM Livro l WHERE l.isbn = :isbn", Livro.class);
-        query.setParameter("isbn", isbn);
-        return query.getSingleResult();
+    public List<Livro> buscarTodos() throws RepositoryException {
+        List<Livro> livros = new ArrayList<Livro>();
+        String sql = "SELECT * FROM livro";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                livros.add(new Livro(
+                		rs.getInt("id"),
+                        rs.getString("titulo"),
+                        rs.getString("autor"),
+                        rs.getString("editora"),
+                        rs.getString("status"),
+                        rs.getInt("nota"),
+                        rs.getString("anotacao"),
+                        rs.getString("isbn")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RepositoryException("Error fetching all books", e);
+        }
+        return livros;
     }
 
-    @Override
-    public List<Livro> buscarTodos() {
-        TypedQuery<Livro> query = entityManager.createQuery("SELECT l FROM Livro l", Livro.class);
-        return query.getResultList();
+    public void atualizar(Livro livro) throws RepositoryException {
+        String sql = "UPDATE livro SET autor = ?, editora = ?, WHERE titulo = ?";
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, livro.getAutor());
+            stmt.setString(2, livro.getEditora());
+            stmt.setString(3, livro.getTitulo());
+            stmt.setString(4, livro.getStatus());
+            stmt.setInt(5, livro.getNota());
+            stmt.setString(6, livro.getAnotacao());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RepositoryException("Error updating the book", e);
+        }
     }
 
-    @Override
-    public void atualizar(Livro livro) {
-        entityManager.getTransaction().begin();
-        entityManager.merge(livro);
-        entityManager.getTransaction().commit();
+    public void deletarLivro(int id) throws RepositoryException {
+        try {
+        	Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        	String sql = "DELETE FROM biblioteca.livro WHERE id = ?";
+        	PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RepositoryException("Error deleting the book", e);
+        }
+    }
+    
+    public Livro buscarLivroInfo(int id) throws RepositoryException {
+    	String sql = "SELECT * FROM biblioteca.livro WHERE id = ?";
+    	List<Livro> livros = new ArrayList<Livro>();
+        try {
+        	Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        	PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+            	Livro livro = new Livro(
+            			rs.getInt("id"),
+                        rs.getString("titulo"),
+                        rs.getString("autor"),
+                        rs.getString("editora"),
+                        rs.getString("status"),
+                        rs.getInt("nota"),
+                        rs.getString("anotacao"),
+                        rs.getString("isbn")
+            			);
+            	livros.add(livro);
+            }
+            
+              
+        } catch (SQLException e) {
+
+            throw new RepositoryException("Error fetching the book by ID", e);
+//            throw new RuntimeException(e.getMessage());
+        }
+        
+        return livros.get(0);
+    }
+    
+    public void editarLivro(Livro livroAtualizado) throws RepositoryException {
+        try {
+        	Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        	String sql = "UPDATE biblioteca.livro SET titulo = ?, autor = ?, editora = ?, status = ?, nota = ?, anotacao = ?, isbn = ? WHERE id = ?";
+        	PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, livroAtualizado.getTitulo());
+            stmt.setString(2, livroAtualizado.getAutor());
+            stmt.setString(3, livroAtualizado.getEditora());
+            stmt.setString(4, livroAtualizado.getStatus());
+            stmt.setInt(5, livroAtualizado.getNota());
+            stmt.setString(6, livroAtualizado.getAnotacao());
+            stmt.setString(7, livroAtualizado.getIsbn());
+            stmt.setInt(8, livroAtualizado.getId());
+            System.out.println(stmt.toString());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+
+            throw new repository.RepositoryException("Error updating the book", e);
+        }
     }
 
-    @Override
-    public void deletar(int id) {
-        Livro livro = entityManager.find(Livro.class, id);
-        entityManager.getTransaction().begin();
-        entityManager.remove(livro);
-        entityManager.getTransaction().commit();
-    }
 }
