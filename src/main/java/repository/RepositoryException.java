0941cae4bc96a9893aa2b12d2ad20b6b34d126ab
Robert França <robertalan.dsf@gmail.com>
2024-08-28 03:08:21 -0300

    Repository excecao.
    Tratamento de excecoes em repository.

---
+++ src/main/java/repository/RepositoryException.java
@@ -0,0 +1,11 @@
+package repository;
+
+public class RepositoryException extends Exception {
+//    public RepositoryException(String message) {
+//        super(message);
+//    }
+
+    public RepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
